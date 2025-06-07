c791371dc3428792d2984ba2e32e4b21c014ffc0
lokeshrookie <rocklinglokesh@gmail.com>
2025-06-07 22:40:51 +0530

    Order service => OrdersController

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private static readonly List<Order> orders = new()
+        {
+            new Order(1,1,2, DateTime.UtcNow.AddDays(-1)),
+            new Order(2,2,3, DateTime.UtcNow.AddHours(-2))
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetOrders()
+        {
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id)
+        {
+            var order = orders.FirstOrDefault(o => o.Id == id);
+
+            if(order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+    }
+}
---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderService.Models
+{
+    //public class Order
+    //{
+    //}
+    public record Order(int Id, int ProductId, int Quantity, DateTime OrderDate);
+
+}
